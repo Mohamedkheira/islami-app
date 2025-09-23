dc49c203ee13cd0ef5ccfb8cbac5e4d0d1970177
MohamedKheira <mohamedkhieraa@gmail.com>
2025-09-23 08:59:47 +0300

    feat: hadeth details logic feat: hadeth tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:isalmi/style/theme_data.dart';
 import 'package:isalmi/ui/chapter_details/chapter_details_screen.dart';
+import 'package:isalmi/ui/hadeth_details/hadeth_details_screen.dart';
 import 'package:isalmi/ui/home/home_screen.dart';
 import 'package:isalmi/ui/splash/splash_screen.dart';
 
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         SplashScreen.routeName : (_) => SplashScreen(),
         HomeScreen.routeName : (_) => HomeScreen(),
         ChapterDetailsScreen.routeName : (_) => ChapterDetailsScreen(),
+        HadethDetailsScreen.routeName : (_) => HadethDetailsScreen(),
       },
       initialRoute: SplashScreen.routeName,
     );
